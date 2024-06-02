80a189f2f55c73ee1af0ad1562c2cd21fc93dbe8
ern23zc <U202112907@upc.edu.pe>
2024-06-02 18:50:56 -0500

    feat Interfaz Usuario

--- src/main/java/com/BookFlow/usuarios/repository/usuarioRepository.java
+++ src/main/java/com/BookFlow/usuarios/repository/usuarioRepository.java
@@ -4,6 +4,8 @@ import com.BookFlow.usuarios.domain.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 @Repository
 public interface usuarioRepository extends JpaRepository<usuario, Long> {
+    Optional<usuario> findByEmail(String email);
 }
