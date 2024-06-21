dbe5ef5685f4502ad8ff62cf58501912e57e3c98
joqui4 <U202211846@upc.edu.pe>
2024-06-21 16:45:17 -0500

    first commit

---
+++ src/main/java/com/BookFlow/usuarios/iam/application/internal/commandservices/RoleCommandServiceImpl.java
@@ -0,0 +1,31 @@
+package com.BookFlow.usuarios.iam.application.internal.commandservices;
+
+import com.BookFlow.usuarios.iam.domain.model.commands.SeedRolesCommand;
+import com.BookFlow.usuarios.iam.domain.model.entities.Role;
+import com.BookFlow.usuarios.iam.domain.model.valueobjects.Roles;
+import com.BookFlow.usuarios.iam.domain.services.RoleCommandService;
+import com.BookFlow.usuarios.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
+
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class RoleCommandServiceImpl implements RoleCommandService {
+    private final RoleRepository roleRepository;
+
+    public RoleCommandServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void handle(SeedRolesCommand command) {
+        Arrays.stream(Roles.values()).forEach(role -> {
+            if (!roleRepository.existsByName(role)) {
+                roleRepository.save(new Role(Roles.valueOf(role.name())));
+            }
+        });
+    }
+}
